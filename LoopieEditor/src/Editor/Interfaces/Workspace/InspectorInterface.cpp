b11e3c0570706caca741be56cb87180ed804fc59
digui048 <digui048@gmail.com>
2026-01-20 00:17:50 +0100

    Have at inspector the Rect Transform

--- LoopieEditor/src/Editor/Interfaces/Workspace/InspectorInterface.cpp
+++ LoopieEditor/src/Editor/Interfaces/Workspace/InspectorInterface.cpp
@@ -423,6 +423,44 @@ namespace Loopie {
 		}
 	}
 
+	void InspectorInterface::DrawRectTransform(RectTransform* rectTransform)
+	{
+		if (ImGui::CollapsingHeader("Rect Transform", ImGuiTreeNodeFlags_DefaultOpen))
+		{
+			if (ImGui::DragFloat2("Anchored Pos", &rectTransform->anchoredPosition.x, 1.0f)) {
+				rectTransform->CalculateRect(); // Recalculate immediately
+			}
+
+			if (ImGui::DragFloat2("Size Delta", &rectTransform->sizeDelta.x, 1.0f)) {
+				rectTransform->CalculateRect();
+			}
+
+			ImGui::Separator();
+
+			if (ImGui::DragFloat2("Anchor Min", &rectTransform->anchorMin.x, 0.01f, 0.0f, 1.0f)) {
+				rectTransform->CalculateRect();
+			}
+
+			if (ImGui::DragFloat2("Anchor Max", &rectTransform->anchorMax.x, 0.01f, 0.0f, 1.0f)) {
+				rectTransform->CalculateRect();
+			}
+
+			if (ImGui::DragFloat2("Pivot", &rectTransform->pivot.x, 0.01f, 0.0f, 1.0f)) {
+				rectTransform->CalculateRect();
+			}
+
+			ImGui::Separator();
+
+			ImGui::Checkbox("Draggable", &rectTransform->draggable);
+			ImGui::SameLine();
+			ImGui::Checkbox("Interactive", &rectTransform->interactive);
+
+			ImGui::Checkbox("Invisible", &rectTransform->invisible);
+			ImGui::SameLine();
+			ImGui::Checkbox("Cut Childs", &rectTransform->cut_childs);
+		}
+	}
+
 	void InspectorInterface::AddComponent(const std::shared_ptr<Entity>& entity)
 	{
 		if (!entity)
